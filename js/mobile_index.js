5c363500fd8440aeeaf884417a8429356207d7ff
HaeKwang Dan <gorhkd0612@gmail.com>
2018-08-08 15:15:36 +0900

    Day16 Finished

--- js/mobile_index.js
+++ js/mobile_index.js
@@ -1,18 +1,7 @@
-function resizeContentBox() {
-    if($("html").width()<=600){
-        $(".videoGroup").addClass("verySmall"); 
-    }else{
-        $(".videoGroup").removeClass("verySmall");
-    }
-}
-
-resizeContentBox();
-
 $(window).resize(function() {
     $(".videoInner").css('left','0');
     $("span.arrowLeft").hide(); 
     $("span.arrowRight").show(); 
-    resizeContentBox();
 });
 
 $("span.arrowLeft").click(function(){
@@ -32,37 +21,3 @@ $("span.arrowRight").click(function(){
     $(this).siblings(".videoInner").animate({left: videoWidth});
     $(this).siblings("span.arrowLeft").show();
 }); 
-
-
-function toggleMenu() {
-    let e = $('#sideBar');
-    let e2 = $('#contents');
-
-    e.toggleClass("forSmall show-for-large").css("visibility", "");
-    e2.toggleClass("blurBox");
-
-}
-
-function toggleSearchBar() {
-    let e = $('#searchBar');
-    let e2 = $('#logoBox');
-    let e3 = $('#menuBox');
-
-    e.toggle();
-    e2.toggle();
-    e3.toggle();
-
-    $("#searchBar input").focus();
-}
-
-$("#sideMenuBtn").click(function () {
-    toggleMenu();
-});
-
-$("#searchBtn").click(function () {
-    toggleSearchBar();
-});
-
-$("#returnSearchBarBtn").click(function () {
-    toggleSearchBar();
-});
---
+++ js/mobile_main.js
@@ -0,0 +1,46 @@
+function resizeContentBox() {
+    if($("html").width()<=600){
+        $(".videoGroup").addClass("verySmall"); 
+    }else{
+        $(".videoGroup").removeClass("verySmall");
+    }
+}
+
+resizeContentBox();
+
+$(window).resize(function() {
+    resizeContentBox();
+});
+
+function toggleMenu() {
+    let e = $('#sideBar');
+    let e2 = $('#contents');
+
+    e.toggleClass("forSmall show-for-large").css("visibility", "");
+    e2.toggleClass("blurBox");
+
+}
+
+function toggleSearchBar() {
+    let e = $('#searchBar');
+    let e2 = $('#logoBox');
+    let e3 = $('#menuBox');
+
+    e.toggle();
+    e2.toggle();
+    e3.toggle();
+
+    $("#searchBar input").focus();
+}
+
+$("#sideMenuBtn").click(function () {
+    toggleMenu();
+});
+
+$("#searchBtn").click(function () {
+    toggleSearchBar();
+});
+
+$("#returnSearchBarBtn").click(function () {
+    toggleSearchBar();
+});

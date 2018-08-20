218f4dcd1e9d59cec9d1202bf2c0a3af9c56e46e
HaeKwang Dan <gorhkd0612@gmail.com>
2018-08-20 12:00:46 +0900

    Revert "Day21 Finished"
    This reverts commit ad1bbff57a7250036191cfdcef295d7a32d5754b.

--- js/mobile_index.js
+++ js/mobile_index.js
@@ -5,19 +5,19 @@ $(window).resize(function() {
 });
 
 $("span.arrowLeft").click(function(){
-    if($(this).siblings(".videoInner").css("left")=="-433px"){
+    if($(this).siblings(".videoInner").css("left")=="-435px"){
         $(this).hide();
     }
-    let videoWidth = "+=433px";
+    let videoWidth = "+=435px";
     $(this).siblings(".videoInner").animate({left: videoWidth});
     $(this).siblings("span.arrowRight").show();
 }); 
 
 $("span.arrowRight").click(function(){
-    if($(this).siblings(".videoInner").css("left")=="-433px"){
+    if($(this).siblings(".videoInner").css("left")=="-435px"){
         $(this).hide();
     }
-    let videoWidth = "-=433px";
+    let videoWidth = "-=435px";
     $(this).siblings(".videoInner").animate({left: videoWidth});
     $(this).siblings("span.arrowLeft").show();
 }); 
